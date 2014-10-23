892dfc3dcf776e971279c20d2442c54e6b34908e
Daniel Lamb <daniellmb@hotmail.com>
2014-10-23 16:15:11 -0700

    formatting to make link happy

--- downlinkmax.js
+++ downlinkmax.js
@@ -2,17 +2,16 @@
  * @file downlinkMax is 0.26KB Network Information API downlinkMax polyfill micro-library.
  * @author Daniel Lamb <dlamb.open.source@gmail.com>
  */
-
 function downlinkmax() {
   var limitless = Infinity,
-      nav = navigator,
-      speed,
+    nav = navigator,
+    speed,
 
-      // deal with vendor prefixing and fallback if not supported
-      connection = nav.connection || nav.mozConnection || nav.webkitConnection || {
-        // API not supported
-        downlinkMax: limitless
-      };
+  // deal with vendor prefixing and fallback if not supported
+    connection = nav.connection || nav.mozConnection || nav.webkitConnection || {
+      // API not supported
+      downlinkMax: limitless
+    };
 
   // check that the API doesn't already support downlinkMax
   if (!('downlinkMax' in connection)) {
@@ -30,16 +29,32 @@ function downlinkmax() {
       // convert connection.type value to approximate downlink values
       // speed estimate is based on the median downlink value for common devices in megabits per second (Mbit/s)
       switch (connection.type) {
-        case 'none': speed = 0; break;
-        case '2g': speed = 0.134; break;
+        case 'none':
+          speed = 0;
+          break;
+        case '2g':
+          speed = 0.134;
+          break;
         case 'bluetooth':
-        case 'cellular': speed = 2; break;
-        case '3g': speed = 8.95; break;
-        case '4g': speed = 100; break;
-        case 'ethernet': speed = 550; break;
-        case 'wifi': speed = 600; break;
+        case 'cellular':
+          speed = 2;
+          break;
+        case '3g':
+          speed = 8.95;
+          break;
+        case '4g':
+          speed = 100;
+          break;
+        case 'ethernet':
+          speed = 550;
+          break;
+        case 'wifi':
+          speed = 600;
+          break;
         // other, unknown etc.
-        default: speed = limitless; break;
+        default:
+          speed = limitless;
+          break;
       }
       connection.downlinkMax = speed;
     }
